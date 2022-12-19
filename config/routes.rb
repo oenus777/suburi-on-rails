29789a8874c2b4204fcd1dfde6e274f77549505d
oenus777 <takatunejp@gmail.com>
2022-12-19 12:34:58 +0000

    [add] rails g controller Users new create edit update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
